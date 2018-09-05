37873a161cfa2c63356c0587cb0c679fe081ace7
caiopotter <limapotter@gmail.com>
2018-09-05 19:58:21 -0300

    Refatoração no trecho de código referente aos ActionListeners.

--- src/buscaBinaria/BuscaBinariaMain.java
+++ src/buscaBinaria/BuscaBinariaMain.java
@@ -26,31 +26,35 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class BuscaBinariaMain extends JFrame implements ActionListener{
+public class BuscaBinariaMain extends JFrame{
 	
+	private static final String INFO_AUTOR = "Criado por Caio Potter \n limapotter@gmail.com";
 	private static final String ERRO_OCORRIDO = "Um erro ocorreu \n";
 	private static final String NOME_ARQUIVO_DAT = "Arquivo dat";
 	private static final String CEP_NÃO_ENCONTRADO = "CEP %s não encontrado.";
 	private static final String CAMINHO_DE_ARQUIVO_VAZIO = "Caminho de arquivo vazio.";
+	private static final int BYTES_POR_REGISTRO_NO_ARQUIVO = 300;
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	static int numeroDeIteracoes = 0;
+
+	static int numeroDeIteracoes;
 	static RandomAccessFile arquivo;
 	static File caminhoJar;
-	
 	static JTextArea areadeTexto;
-	static JTextField textoCaminhoArquivo;
-	static JButton botaoBuscaCEP, botaoBuscaCaminhoArquivo;
-	static JLabel labelCaminhoArquivo, labelCEP;
-	static JScrollPane scrollBar;
-    static JPanel gridPanel, flowPanel;
-    static JFormattedTextField textoCEP;
-    static JMenuBar menuBar;
-    static JMenu menuAjuda;
-    static JMenuItem acaoSobre;
     static JFrame GUI;
+	
+	JTextField textoCaminhoArquivo;
+	JButton botaoBuscaCEP, botaoBuscaCaminhoArquivo;
+	JLabel labelCaminhoArquivo, labelCEP;
+	JScrollPane scrollBar;
+	JPanel gridPanel, flowPanel;
+    JFormattedTextField textoCEP;
+    JMenuBar menuBar;
+    JMenu menuAjuda;
+    JMenuItem acaoSobre;
 
 	public static void main(String[] args) throws Exception{
 		
@@ -61,33 +65,49 @@ public class BuscaBinariaMain extends JFrame implements ActionListener{
 	
 	public BuscaBinariaMain() throws ParseException, UnsupportedEncodingException, URISyntaxException {
 		caminhoJar = recebeCaminhoJar();
-		gridPanel = new JPanel(new GridLayout(2, 3));
-		flowPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		setSize(500,230);
-        setTitle("Busca Binária");
-
+		definePropriedadesJanela();
         instanciarComponentes();
         incluirListeners();
         adicionarComponentes();
 	}
 
+	private void definePropriedadesJanela() {
+		gridPanel = new JPanel(new GridLayout(2, 3));
+		flowPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		setSize(500,230);
+        setTitle("Busca Binária");
+	}
+
 	private void incluirListeners() {
-		botaoBuscaCaminhoArquivo.addActionListener(this);
-		botaoBuscaCEP.addActionListener(this);
-		acaoSobre.addActionListener(this);
+		botaoBuscaCaminhoArquivo.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) {
+				  executaAcaoBuscaCaminhoArquivo();
+			  }
+		});
+		
+		botaoBuscaCEP.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) {
+				  executaAcaoBuscaCEP();
+			  }
+		});
+		acaoSobre.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) {
+				  executaAcaoMenuSobre();
+			  }
+		});
 	}
 
 	private void instanciarComponentes() throws ParseException {
 		labelCaminhoArquivo = new JLabel("Arquivo .dat:");
+		labelCEP = new JLabel("Digite o CEP:");
         textoCaminhoArquivo = new JTextField();
-        botaoBuscaCaminhoArquivo = new JButton("Selecionar arquivo");
-        labelCEP = new JLabel("Digite o CEP:");
-        javax.swing.text.MaskFormatter formatacaoCEP = new javax.swing.text.MaskFormatter("#####-###");
-        textoCEP = new javax.swing.JFormattedTextField(formatacaoCEP);
-        botaoBuscaCEP = new JButton("Buscar CEP");
         areadeTexto = new JTextArea(6, 40);
         areadeTexto.setEditable(false);
         scrollBar = new JScrollPane(areadeTexto);
+        javax.swing.text.MaskFormatter formatacaoCEP = new javax.swing.text.MaskFormatter("#####-###");
+        textoCEP = new javax.swing.JFormattedTextField(formatacaoCEP);
+        botaoBuscaCEP = new JButton("Buscar CEP");
+        botaoBuscaCaminhoArquivo = new JButton("Selecionar arquivo");
         menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         menuAjuda = new JMenu("Ajuda");
@@ -120,14 +140,14 @@ public class BuscaBinariaMain extends JFrame implements ActionListener{
 	private static void buscaBinaria(RandomAccessFile arquivo, String cepDigitado, Long inicio, Long fim) throws IOException {
 		Endereco endereco = new Endereco();
 		numeroDeIteracoes++;
-		arquivo.seek(((inicio+fim)/2)*300);
+		arquivo.seek(((inicio+fim)/2)*BYTES_POR_REGISTRO_NO_ARQUIVO);
 		endereco.leEndereco(arquivo);
 		if(endereco.getCep().equals(cepDigitado)) {
 			escreverResultado(endereco);
 		}else if(cepDigitado.compareTo(endereco.getCep()) > 0) {
-			buscaBinaria(arquivo, cepDigitado, (arquivo.getFilePointer()+1)/300, fim);
+			buscaBinaria(arquivo, cepDigitado, (arquivo.getFilePointer()+1)/BYTES_POR_REGISTRO_NO_ARQUIVO, fim);
 		}else {
-			buscaBinaria(arquivo, cepDigitado, inicio, (arquivo.getFilePointer()-1)/300);
+			buscaBinaria(arquivo, cepDigitado, inicio, (arquivo.getFilePointer()-1)/BYTES_POR_REGISTRO_NO_ARQUIVO);
 		}
 	}
 
@@ -139,39 +159,36 @@ public class BuscaBinariaMain extends JFrame implements ActionListener{
 		areadeTexto.append("\n" + "Estado: " + endereco.getEstado());
 		areadeTexto.append("\n" + "Numero de iterações na busca binária: " + numeroDeIteracoes);
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent evento) {
-		if(evento.getSource() == acaoSobre) {
-			JOptionPane.showMessageDialog(GUI, "Criado por Caio Potter \n limapotter@gmail.com");
-		}
-		if(evento.getSource() == botaoBuscaCEP) {
-			String cep = textoCEP.getText().replace("-", "");
-			try {
-				arquivo = selecionarArquivo(textoCaminhoArquivo.getText());
-				numeroDeIteracoes = 0;
-				buscaBinaria(arquivo, cep, 0L, (arquivo.length()/300));
-				arquivo.close();
-			} catch (IOException e) {
-				areadeTexto.setText(ERRO_OCORRIDO);
-				if(textoCaminhoArquivo.getText().isEmpty()) {
-					areadeTexto.append(CAMINHO_DE_ARQUIVO_VAZIO);
-				}
-				areadeTexto.append(e.getMessage());
-			} catch (StackOverflowError e) {
-				areadeTexto.setText(String.format(CEP_NÃO_ENCONTRADO, textoCEP.getText()));
+	
+	public void executaAcaoBuscaCEP() {
+		String cep = textoCEP.getText().replace("-", "");
+		try {
+			arquivo = selecionarArquivo(textoCaminhoArquivo.getText());
+			numeroDeIteracoes = 0;
+			buscaBinaria(arquivo, cep, 0L, (arquivo.length()/BYTES_POR_REGISTRO_NO_ARQUIVO));
+			arquivo.close();
+		} catch (IOException e) {
+			areadeTexto.setText(ERRO_OCORRIDO);
+			if(textoCaminhoArquivo.getText().isEmpty()) {
+				areadeTexto.append(CAMINHO_DE_ARQUIVO_VAZIO);
 			}
+			areadeTexto.append(e.getMessage());
+		} catch (StackOverflowError e) {
+			areadeTexto.setText(String.format(CEP_NÃO_ENCONTRADO, textoCEP.getText()));
 		}
-		if(evento.getSource() == botaoBuscaCaminhoArquivo) {
-			final JFileChooser fc = new JFileChooser();
-			fc.setCurrentDirectory(caminhoJar);
-			fc.setFileFilter(new FileNameExtensionFilter(NOME_ARQUIVO_DAT, "dat"));
-			int returnVal = fc.showOpenDialog(GUI);
-			if(returnVal == JFileChooser.APPROVE_OPTION) {
-				textoCaminhoArquivo.setText(fc.getSelectedFile().getPath());
-			}
-		}
-		
 	}
-
+	
+	public void executaAcaoMenuSobre() {
+		JOptionPane.showMessageDialog(GUI, INFO_AUTOR);
+	}
+	
+	public void executaAcaoBuscaCaminhoArquivo() {
+		final JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(caminhoJar);
+		fc.setFileFilter(new FileNameExtensionFilter(NOME_ARQUIVO_DAT, "dat"));
+		int returnVal = fc.showOpenDialog(GUI);
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			textoCaminhoArquivo.setText(fc.getSelectedFile().getPath());
+		}
+	}
 }
